d39f5f565257efdc54e6dd421f362e42f0f7434d
Vishwapratap5 <revandeguru@gmail.com>
2024-12-31 17:00:27 +0530

    moved folders

--- Advance Java Tutorials/untitled/src/package1/Constants.java
+++
@@ -1,7 +0,0 @@
-package package1;
-
-public class Constants {
-    public static final String USERNAME="root";
-    public static final  String PASSWORD="vishwanj@2316";
-    public static final  String URL="jdbc:mysql://127.0.0.1:3306/hotel_db";
-}
